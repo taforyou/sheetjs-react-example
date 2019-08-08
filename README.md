78f0749d94f5b54e15da2479528765b614e9cb05
Chanon Yaklai <taforyou@hotmail.com>
2019-08-08 12:20:15 +0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## Just Create React App With SheetJS Community
+
+The library can also be imported directly from JSX code with:
+
+```js
+import XLSX from "xlsx";
+```
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SheetJSApp from "./sheetjs.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <SheetJSApp />
+    </div>
+  );
+}
+
+export default App;
